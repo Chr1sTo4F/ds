4c47a15ecb7131babf3c921bc2733b87f4078022
0000FF <117788769+Chr1sTo4F@users.noreply.github.com>
2023-09-17 08:21:21 +0000

    modified:   2.3.7_24.c

--- 2.3.7_24.c
+++ 2.3.7_24.c
@@ -1,4 +1,6 @@
 //核心思想：空间换时间
+#include <stdio.h>
+
 typedef struct node{
     int data;
     struct node *link;
@@ -8,5 +10,20 @@ typedef NODE *PNODE;
 void func (PNODE h,int n){
     PNODE p=h,r;
     int *q,m;
-    
+    q=(int *)malloc(sizeof(int)*(n+1));
+    for(int i=0;i<n+1;i++)          //initialize-->0
+        *(q+1)=0;
+    while(p->link!=NULL){
+        m=p->link->data>0?p->link->data:-p->link->data;
+        if(*(q+m)==0){
+            *(q+m)=1;
+            p=p->link;
+        }
+        else{
+            r=p->link;
+            p->link=r->link;
+            free(r);
+        }
+        free(q);
+    }
 }
